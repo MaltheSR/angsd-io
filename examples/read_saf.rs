1fc0dcc647148f65eeda533932bf2c19937b1a1d
Malthe Rasmussen <malthesebro@protonmail.com>
2021-01-12 13:55:28 +0100

    doc(saf): Add example of reading SAF file

---
+++ examples/read_saf.rs
@@ -0,0 +1,29 @@
+use std::{
+    env,
+    io::{self, Write}
+};
+
+use angsd_io::saf::reader::Reader;
+
+fn main() -> io::Result<()> {
+    let src = env::args().nth(1).expect("missing path to SAF member file");
+
+    let mut reader = Reader::from_member_path(src)?;
+
+    let stdout = io::stdout();
+    let mut writer = io::BufWriter::new(stdout.lock());
+
+    writeln!(&mut writer, "{}", reader.index())?;
+
+    let header = format!  (
+        "chrom\tpos\t{}",
+        (0..reader.index().n_alleles()).map(|x| x.to_string()).collect::<Vec<String>>().join("\t")
+    );
+    writeln!(&mut writer, "{}", header)?;
+
+    for site in reader.sites() {
+        writeln!(&mut writer, "{}", site?)?;
+    }
+
+    Ok(())
+}
