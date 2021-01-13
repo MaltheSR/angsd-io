f9019332587adbd71a0267f33fe6cacd98ba83de
Malthe Rasmussen <malthesebro@protonmail.com>
2021-01-13 17:42:21 +0100

    doc: Change output format to fit merged SAF example

--- examples/read_saf.rs
+++ examples/read_saf.rs
@@ -8,20 +8,15 @@ use angsd_io::saf::reader::Reader;
 fn main() -> io::Result<()> {
     let src = env::args().nth(1).expect("missing path to SAF member file");
 
-    let mut reader = Reader::from_member_path(src)?;
+    let mut reader = Reader::from_member_path(&src)?;
 
     let stdout = io::stdout();
     let mut writer = io::BufWriter::new(stdout.lock());
 
     writeln!(&mut writer, "{}", reader.index())?;
 
-    let header = format!(
-        "chrom\tpos\t{}",
-        (0..reader.index().n_alleles())
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join("\t")
-    );
+    let header = format!("chrom\tpos\t{}", src);
+
     writeln!(&mut writer, "{}", header)?;
 
     for site in reader.sites() {
