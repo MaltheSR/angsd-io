70111002357259a215e2f06e56af6e1f7c547d0f
Malthe Rasmussen <malthesebro@protonmail.com>
2021-01-24 15:59:46 +0100

    refactor(saf::read): Split main Reader into separate file

--- src/saf/read.rs
+++ src/saf/read.rs
@@ -1,151 +1,17 @@
-use std::{
-    fs::File,
-    io,
-    path::{Path, PathBuf},
-};
-
-#[cfg(feature = "ndarray")]
-use ndarray::prelude::*;
-
-use crate::saf::{constants::*, index};
+use std::io;
 
 pub mod iter;
 mod merged_reader;
 mod position_reader;
+mod reader;
 mod value_reader;
 
 pub use self::{
-    merged_reader::MergedReader, position_reader::PositionReader, value_reader::ValueReader,
+    merged_reader::MergedReader, position_reader::PositionReader, reader::Reader,
+    value_reader::ValueReader,
 };
 
-pub struct Reader<R> {
-    index: index::Index,
-    position_reader: PositionReader<R>,
-    value_reader: ValueReader<R>,
-}
-
-impl<R> Reader<R> {
-    pub fn index(&self) -> &index::Index {
-        &self.index
-    }
-
-    pub fn index_mut(&mut self) -> &mut index::Index {
-        &mut self.index
-    }
-
-    pub fn new(
-        index: index::Index,
-        position_reader: PositionReader<R>,
-        value_reader: ValueReader<R>,
-    ) -> Self {
-        Self {
-            index,
-            position_reader,
-            value_reader,
-        }
-    }
-
-    pub fn position_reader_mut(&mut self) -> &mut PositionReader<R> {
-        &mut self.position_reader
-    }
-
-    pub fn value_reader_mut(&mut self) -> &mut ValueReader<R> {
-        &mut self.value_reader
-    }
-}
-
-impl<R> Reader<R>
-where
-    R: io::BufRead,
-{
-    #[cfg(feature = "ndarray")]
-    pub fn read_values_to_array(mut self) -> io::Result<Array2<f32>> {
-        let n_sites = self.index.n_sites();
-        let n_alleles = self.index.n_alleles();
-
-        let mut values = Vec::new();
-        values.resize(n_sites * n_alleles, 0.0);
-
-        self.value_reader
-            .read_exact(values.as_mut_slice())
-            .map_err(|_| {
-                io::Error::new(
-                    io::ErrorKind::InvalidData,
-                    "SAF value dimensions do not match metadata",
-                )
-            })?;
-
-        let array = Array::from_shape_vec((n_sites, n_alleles), values).unwrap();
-
-        Ok(array)
-    }
-
-    pub fn sites(&mut self) -> iter::Sites<R> {
-        iter::Sites::new(
-            self.index.iter_names(),
-            self.position_reader.iter(),
-            self.value_reader.chunks(self.index.n_alleles()),
-        )
-    }
-
-    pub fn read_header(&mut self) -> io::Result<String> {
-        // Ok to unwrap here, magic is checked in index construction
-        let index_header = parse_magic(&self.index.magic())?;
-        let position_header = self.position_reader.read_header()?;
-        let value_header = self.value_reader.read_header()?;
-
-        assert!(index_header == position_header && index_header == value_header);
-
-        Ok(index_header)
-    }
-}
-
-impl Reader<io::BufReader<File>> {
-    pub fn from_paths<P>(index_path: P, position_path: P, value_path: P) -> io::Result<Self>
-    where
-        P: AsRef<Path>,
-    {
-        let index = File::open(index_path).map(|x| index::Reader::new(x).read_index())??;
-
-        let position_reader = PositionReader::from_path(position_path)?;
-
-        let value_reader = ValueReader::from_path(value_path)?;
-
-        Ok(Self::new(index, position_reader, value_reader))
-    }
-
-    pub fn from_member_path<P>(path: P) -> io::Result<Self>
-    where
-        P: Into<PathBuf>,
-    {
-        let prefix = find_prefix(path).ok_or_else(|| {
-            io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "input is not a SAF member path",
-            )
-        })?;
-
-        Self::from_prefix(prefix)
-    }
-
-    pub fn from_prefix<P>(prefix: P) -> io::Result<Self>
-    where
-        P: Into<PathBuf>,
-    {
-        let prefix = prefix.into();
-
-        let mut index_path = prefix.clone();
-        index_path.set_extension(INDEX_EXT);
-
-        let mut value_path = prefix.clone();
-        value_path.set_extension(VALUE_EXT);
-
-        let mut position_path = prefix;
-        position_path.set_extension(POSITION_EXT);
-
-        Self::from_paths(index_path, position_path, value_path)
-    }
-}
+use crate::saf::constants::MAGIC;
 
 pub trait BinaryRead {
     type Value: Copy + Default;
@@ -161,22 +27,6 @@ pub trait BinaryRead {
     fn iter(&mut self) -> iter::BinaryIterator<Self> {
         iter::BinaryIterator::new(self)
     }
-}
-
-fn find_prefix<P>(path: P) -> Option<String>
-where
-    P: Into<PathBuf>,
-{
-    let string = path
-        .into()
-        .into_os_string()
-        .into_string()
-        .expect("cannot convert path to string");
-
-    EXTENSIONS
-        .iter()
-        .find(|x| string.ends_with(*x))
-        .map(|x| string.trim_end_matches(*x).into())
 }
 
 pub(crate) fn read_magic<R>(reader: &mut R) -> io::Result<([u8; 8])>
---
+++ src/saf/read/reader.rs
@@ -0,0 +1,160 @@
+use std::{
+    fs::File,
+    io,
+    path::{Path, PathBuf},
+};
+
+#[cfg(feature = "ndarray")]
+use ndarray::prelude::*;
+
+use super::{iter, parse_magic, BinaryRead, PositionReader, ValueReader};
+
+use crate::saf::{
+    constants::{EXTENSIONS, INDEX_EXT, POSITION_EXT, VALUE_EXT},
+    index,
+};
+
+pub struct Reader<R> {
+    index: index::Index,
+    position_reader: PositionReader<R>,
+    value_reader: ValueReader<R>,
+}
+
+impl<R> Reader<R> {
+    pub fn index(&self) -> &index::Index {
+        &self.index
+    }
+
+    pub fn index_mut(&mut self) -> &mut index::Index {
+        &mut self.index
+    }
+
+    pub fn new(
+        index: index::Index,
+        position_reader: PositionReader<R>,
+        value_reader: ValueReader<R>,
+    ) -> Self {
+        Self {
+            index,
+            position_reader,
+            value_reader,
+        }
+    }
+
+    pub fn position_reader_mut(&mut self) -> &mut PositionReader<R> {
+        &mut self.position_reader
+    }
+
+    pub fn value_reader_mut(&mut self) -> &mut ValueReader<R> {
+        &mut self.value_reader
+    }
+}
+
+impl<R> Reader<R>
+where
+    R: io::BufRead,
+{
+    #[cfg(feature = "ndarray")]
+    pub fn read_values_to_array(mut self) -> io::Result<Array2<f32>> {
+        let n_sites = self.index.n_sites();
+        let n_alleles = self.index.n_alleles();
+
+        let mut values = Vec::new();
+        values.resize(n_sites * n_alleles, 0.0);
+
+        self.value_reader
+            .read_exact(values.as_mut_slice())
+            .map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "SAF value dimensions do not match metadata",
+                )
+            })?;
+
+        let array = Array::from_shape_vec((n_sites, n_alleles), values).unwrap();
+
+        Ok(array)
+    }
+
+    pub fn sites(&mut self) -> iter::Sites<R> {
+        iter::Sites::new(
+            self.index.iter_names(),
+            self.position_reader.iter(),
+            self.value_reader.chunks(self.index.n_alleles()),
+        )
+    }
+
+    pub fn read_header(&mut self) -> io::Result<String> {
+        // Ok to unwrap here, magic is checked in index construction
+        let index_header = parse_magic(&self.index.magic())?;
+        let position_header = self.position_reader.read_header()?;
+        let value_header = self.value_reader.read_header()?;
+
+        assert!(index_header == position_header && index_header == value_header);
+
+        Ok(index_header)
+    }
+}
+
+impl Reader<io::BufReader<File>> {
+    pub fn from_paths<P>(index_path: P, position_path: P, value_path: P) -> io::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let index = File::open(index_path).map(|x| index::Reader::new(x).read_index())??;
+
+        let position_reader = PositionReader::from_path(position_path)?;
+
+        let value_reader = ValueReader::from_path(value_path)?;
+
+        Ok(Self::new(index, position_reader, value_reader))
+    }
+
+    pub fn from_member_path<P>(path: P) -> io::Result<Self>
+    where
+        P: Into<PathBuf>,
+    {
+        let prefix = find_prefix(path).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "input is not a SAF member path",
+            )
+        })?;
+
+        Self::from_prefix(prefix)
+    }
+
+    pub fn from_prefix<P>(prefix: P) -> io::Result<Self>
+    where
+        P: Into<PathBuf>,
+    {
+        let prefix = prefix.into();
+
+        let mut index_path = prefix.clone();
+        index_path.set_extension(INDEX_EXT);
+
+        let mut value_path = prefix.clone();
+        value_path.set_extension(VALUE_EXT);
+
+        let mut position_path = prefix;
+        position_path.set_extension(POSITION_EXT);
+
+        Self::from_paths(index_path, position_path, value_path)
+    }
+}
+
+fn find_prefix<P>(path: P) -> Option<String>
+where
+    P: Into<PathBuf>,
+{
+    let string = path
+        .into()
+        .into_os_string()
+        .into_string()
+        .expect("cannot convert path to string");
+
+    EXTENSIONS
+        .iter()
+        .find(|x| string.ends_with(*x))
+        .map(|x| string.trim_end_matches(*x).into())
+}
